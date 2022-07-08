f90b42a915dbb2a12b311144a4dbd5363273f87e
heyinbao <heyinbao@imydao.com>
2022-07-08 14:03:43 +0800

    配置文件添加getInt方法和getString方法

--- src/com/surd/one/PropertyMgr.java
+++ src/com/surd/one/PropertyMgr.java
@@ -1,5 +1,7 @@
 package com.surd.one;
 
+import com.sun.deploy.util.StringUtils;
+
 import java.io.IOException;
 import java.util.Properties;
 
@@ -23,6 +25,22 @@ public class PropertyMgr {
             return null;
         }
         return pros.get(key);
+    }
+
+    public static int getInt(String key){
+        String string = getString(key);
+        if (null == string){
+            return 0;
+        }
+        return Integer.valueOf(string);
+    }
+
+    public static String getString(String key){
+        Object o = get(key);
+        if (null == o){
+            return null;
+        }
+        return String.valueOf(o);
     }
 
     public static void main(String[] args) {
