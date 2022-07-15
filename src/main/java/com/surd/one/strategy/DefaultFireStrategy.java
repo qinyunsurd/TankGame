cdd1d08bd0cdb90dcef07856f5dfae092a91cb09
heyinbao <heyinbao@imydao.com>
2022-07-15 18:20:58 +0800

    去除装饰器模式

--- src/main/java/com/surd/one/strategy/DefaultFireStrategy.java
+++ src/main/java/com/surd/one/strategy/DefaultFireStrategy.java
@@ -14,10 +14,10 @@ public class DefaultFireStrategy implements FireStrategy {
         //根据坦克图片大小，计算子弹位置
         int bx = t.x + Tank.TANK_WIDTH/2 - Bullet.BULLET_WIDTH/2;
         int by = t.y + Tank.TANK_HEIGHT/2 - Bullet.BULLET_HEIGHT/2;
-        GameModel.getInstance().add(
-                new RectDecorator(
-                        new TailDecorator(new Bullet(bx,by,t.dir,t.group))));
-
+        //GameModel.getInstance().add(
+        //        new RectDecorator(
+        //                new TailDecorator(new Bullet(bx,by,t.dir,t.group))));
+        new Bullet(bx,by,t.dir,t.group);
         if (t.group == Group.GOOD){
             new Thread(() -> new Audio("audio/tank_fire.wav"));
         }
